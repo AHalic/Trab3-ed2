ef731887f7356c237dc33bcd2fedb674c146b5d3
bea <beamaia.work@gmail.com>
2021-04-19 18:40:55 -0300

    adiciona openFile e readIndex

--- src/utils.c
+++ src/utils.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "hash.h"
+#include "word.h"
 
 char* stringConcat(char* dir, char* file) {
     int dirLen = strlen(dir);
@@ -15,4 +17,30 @@ char* stringConcat(char* dir, char* file) {
     strcat(filePath, fileName);
 
     return filePath;
+}
+
+FILE* openFile(char* dir, char* file){
+    FILE* fp;
+    char* filePath;
+
+    filePath = stringConcat(dir, file);
+    fp = fopen(filePath, "r");
+
+    return fp;
+}
+
+Hash* readIndex(FILE* fp, Hash* hashTable) {
+    char* lineBuffer = NULL;
+    size_t n;
+
+    while(!feof(fp)) {
+        getline(&lineBuffer, &n, fp);
+        lineBuffer[strcspn(lineBuffer, "\r\n")] = '\0';
+
+        Word* auxWord = acess(hashTable, lineBuffer);
+        updateNAppearance(auxWord);
+        // showWordList(auxWord);
+    }
+
+    return hashTable;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdio.h>
+#include "hash.h"
+
 char* stringConcat(char* dir, char* file);
+
+FILE* openFile(char* dir, char* file);
+
+Hash* readIndex(FILE* fp, Hash* hashTable);
 
 #endif
