01ed0204c229332afec35fdfe93eb5a994449647
bea <beamaia.work@gmail.com>
2021-04-19 18:41:12 -0300

    retira parte adicionada a utils

--- src/main.c
+++ src/main.c
@@ -8,30 +8,16 @@
 int main(int argc, char* argv[]) {
     if (argc != 2)
         return 1;
+
     FILE* indexFile,* graphFile,* stopWordsFile;
-    char* indexPath,* graphPath,* stopWordsPath;
-
-    indexPath = stringConcat(argv[1], "index.txt");
-    graphPath = stringConcat(argv[1], "graph.txt");
-    stopWordsPath = stringConcat(argv[1], "stopwords.txt");
-
-    indexFile = fopen(indexPath, "r");
-    graphFile = fopen(graphPath, "r");
-    stopWordsFile = fopen(stopWordsPath, "r");
+    indexFile = openFile(argv[1], "index.txt");
+    graphFile = openFile(argv[1], "graph.txt");
+    stopWordsFile = openFile(argv[1], "stopwords.txt");
 
     Hash* hashFiles = initHash();
-    char* lineBuffer = NULL;
-    size_t n;
 
-    while(!feof(indexFile)) {
-        getline(&lineBuffer, &n, indexFile);
-        lineBuffer[strcspn(lineBuffer, "\r\n")] = '\0';
+    readIndex(indexFile, hashFiles);
+    showHash(hashFiles);    
 
-        Word* auxWord = acess(hashFiles, lineBuffer);
-        updateNAppearance(auxWord);
-        showWordList(auxWord);
-    }
-
-    // showHash(hashFiles);    
     return 0;
 }
