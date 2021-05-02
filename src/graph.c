1427daba49b9b702025a2baf0a198600e452013c
AHalic <sophie.gama@aluno.ufes.br>
2021-05-02 17:28:51 -0300

    Modifica identação

--- src/graph.c
+++ src/graph.c
@@ -1,52 +1,56 @@
+#include "graph.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include "graph.h"
-#include "word.h"
+
+#include "page.h"
 #include "trie.h"
 #include "node.h"
 
-struct graph{
-	Node **nodeArray;
-	int nNodes;
+struct graph {
+    Node **nodeArray;
+    int nNodes;
 };
 
-Graph* initGraph(Node** vector, int count_files){
-	Graph *new = (Graph *)malloc(sizeof(Graph));
-	new->nodeArray = vector;
-	new->nNodes = count_files;
+Graph *initGraph(Node **vector, int count_files){
+    Graph *new = (Graph *)malloc(sizeof(Graph));
+    new->nodeArray = vector;
+    new->nNodes = count_files;
+    return new;
 }
 
-void updatePageRank(Graph* graph){
+void updatePageRank(Graph *graph){
     int tam = graph->nNodes;
-    while (1){
+	
+    while (1) {
         double error = 0;
         for (int i = 0; i < tam; i++){
             // pega o influenciador
-            Node* aux = graph->nodeArray[i];
+            Node *aux = graph->nodeArray[i];
             calcPageRank(aux, tam);
         }
-        for(int i = 0; i < tam; i++){
-            Node* aux = graph->nodeArray[i];
+        for (int i = 0; i < tam; i++) {
+            Node *aux = graph->nodeArray[i];
             double diff = changePRs(aux);
-            error += diff*diff;
+            error += diff * diff;
         }
 
         error = sqrt(error);
-        if(error <= 0.000001){
+        if (error <= 0.000001) {
             break;
         }
-    } 
+    }
 }
 
-void showAllPR(Graph* graph) {
+void showAllPR(Graph *graph) {
     for (int i = 0; i < graph->nNodes; i++) {
         printf("Page: %s: \n", getFileName(graph->nodeArray[i]));
         printf("PR: %lf\n", getPR_old(graph->nodeArray[i]));
     }
 }
 
-void destroyGraph(Graph* graph) {
+void destroyGraph(Graph *graph) {
     destroyNodeVector(graph->nodeArray, graph->nNodes);
     free(graph);
 }
