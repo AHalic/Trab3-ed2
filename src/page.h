bafeb6397e198d84dfbdadcc6de8e4177b3efe76
AHalic <sophie.gama@aluno.ufes.br>
2021-05-07 23:55:30 -0300

    Adiciona comentarios

--- src/page.h
+++ src/page.h
@@ -7,60 +7,67 @@
 typedef struct page Pages;
 
 /*
- * Inicializa a word
- * Input: char*
- * Output: Pages*
- */ 
+ * Inicializa a page
+ * @param {Node*}nodeFile nó que será inserido na Page
+ * @return página criada
+**/ 
 Pages* initPage (Node *nodeFile);
 
 /**
- * Faz uma cópia de nodeList e retorna
- * Input: Pages*
- * Output: Pages*
-*/
+ * Retorna uma cópia de nodeList
+ * @param {Pages*}nodeList lista de páginas que será copiada
+ * @return lista de páginas cópia de nodeList
+**/
 Pages* getPagesCopy(Pages* nodeList);
 
 /**
- * Filtra a lista de páginas baseList usando uma outra lista de páginas filterList (faz a interseção das duas listas)
- * Input: Pages*, Pages*
- * Output: Pages*
-*/
+ * Faz a interseção entre duas listas de páginas
+ * @param {Pages*}baseList
+ * @param {Pages*}filterList
+ * @return retorna uma lista de páginas contendo as páginas 
+ *         presentes nas duas listas de entrada
+**/
 Pages* filterPageList(Pages* baseList, Pages* filterList);
 
 /* 
- * Retorna a string da word
- * Input: Pages*
- * Output: char*
- */ 
+ * Retorna o nome do nó da página
+ * @param {Pages*}page
+ * @return string nome do arquivo
+**/ 
 char* getPageName (Pages* page);
 
+/**
+ * Retorna o page rank do nó da página
+ * @param {Pages*}page
+ * @return double page rank
+**/
 double getPR (Pages* page);
 
 /*
- * Verifica se a word existe dentro da lista de palavras
- * Input: Pages*, char*
- * Output: Pages*
+ * Verifica se o nó existe dentro da lista de pages
+ * @param {Pages*}pal lista de pages
+ * @param {Node*}nodeFile nó a ser buscado
+ * @return page contendo o nó buscado
  */ 
 Pages* searchPage (Pages* pal, Node *nodeFile);
 
 /*
- * Insere word *lista* a word *p* 
- * Input: Pages*, Pages*
- * Output: Pages*
+ * Insere page a uma lista de pages
+ * @param {Pages*}lista
+ * @param {Pages*}p page que será inserida
+ * @return nova lista contendo page
  */ 
 Pages* insertPage (Pages* lista, Pages* p);
 
 /*
- * Imprime lista de palavras
- * Input: Pages*
- * Output: void
+ * Imprime lista de pages
+ * @param {Pages*}pal lista de pages
  */ 
 void showPageList (Pages* pal);
 
 /*
- * Libera memoria alocada pela lista de palavras
- * Input: Pages*
- * Output: void
+ * Libera memoria alocada pela lista de pages
+ * @param {Pages*}pal
  */ 
 void destroyPageList (Pages* pal);
 
