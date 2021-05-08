8bf7e5e8657165ffa66ec8c991e933eed918218c
bea <beamaia.work@gmail.com>
2021-05-07 23:48:34 -0300

    adiciona comentarios

--- src/hash.h
+++ src/hash.h
@@ -4,24 +4,26 @@
 
 typedef struct hash Hash;
 
-/*
+/**
  * Inicializa a tabela hash com o tamanho informado e todos elementos NULL
- * Input: void
- * Output: Hash*
+ * @param {int}sz tamanho do vetor da hash
+ * 
+ * @return retorna hash inicializada
  */ 
 Hash* initHash (int sz);
 
-/*
+/**
  * Atualiza a tabela Hash
- * Input: Hash*, char*
- * Output: Pages*
+ * @param {Hash*}h hash inicializada
+ * @param {char*}string string da palavra para acessar 
+ * 
+ * @return Node em que ha a string
  */ 
 Node* accessHash (Hash* h, char* string);
 
-/*
+/**
  * Libera a memoria alocada pela tabela hash
- * Input: Hash*
- * Output: void
+ * @param {Hash*}h hash inicializada
  */ 
 void destroyHash (Hash* h);
 
